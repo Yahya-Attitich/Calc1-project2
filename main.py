24d0671cc1792cb652a3eff9d6877d80ba7e027c
makenzie orme <makenzieorme@live.com>
2024-03-26 17:54:39 -0600

    added task 3 and 4

--- main.py
+++ main.py
@@ -9,6 +9,33 @@ test_radius = [2, 4, 5, 10]
 for radius in test_radius:
     print(f"For a sphere with a radius of '{radius}' the volume is: '{calculate_sphere_volume(radius)}'.")
 
+# Task 3 Derivatives!
+def derivative(f, x):
+    h = 1e-6
+    return round(((f(x+h) - f(x))/h), 5)
+
+# Task 3 Demonstration
+x = 1
+test_derivative = derivative((lambda x : 3 * x**2 - (6 * x) + 1), x)
+print(f"The slope of (3x)^2 - 6x + 1 at x = {x} is {test_derivative}")
+
+# Task 4 quadratic formula
+def quadratic(a, b, c):
+    numerator = b**2 - 4 * a * c 
+    if numerator < 0:
+        return []
+    elif numerator == 0:
+        root = -b / (2 * a)
+        return [root]
+    else:
+        root1 = (-b + math.sqrt(numerator)) / (2 * a)
+        root2 = (-b - math.sqrt(numerator)) / (2 * a)
+        return [root1, root2]
+    
+# Task 4 Demonstration
+roots = quadratic(3, -4, 1)
+print(f"the roots of 3x^2 -4x + 1 are {roots[0]} and {roots[1]}")
+
 # Task 6: Riemman Summation
 def left_riemman_sum(f, dx, start, end):
     sum = 0
